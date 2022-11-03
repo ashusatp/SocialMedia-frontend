c9ef2c63e611d6e583d66b39ad32aa4ad8a78117
ashusatp <92241390+ashusatp@users.noreply.github.com>
2022-11-03 20:05:27 +0530

    stage all changes again (#1)
    * stage all changes again
    
    * created NavBar and leftBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
 import Login from "./Pages/login/Login";
-import Register from "./Pages/register/Register"
+import Register from "./Pages/register/Register";
+import Profile from "./Pages/profile/Profile";
+import Home from "./Pages/home/Home";
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
+  Outlet,
+  Navigate,
 } from "react-router-dom";
+import NavBar from "./components/navBar/NavBar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
 function App() {
+  const currentUser = true;
+
+  const Layout = () => {
+    return (
+      <div>
+        <NavBar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{flex: 6}}>
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProctectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
   const router = createBrowserRouter([
     {
+      path: "/",
+      element: (
+        <ProctectedRoute>
+          <Layout />
+        </ProctectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
       path: "/login",
-      element: <Login/>,
+      element: <Login />,
     },
     {
       path: "/register",
-      element: <Register/>,
+      element: <Register />,
     },
   ]);
   return (
     <div>
-        <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/rightBar/RightBar.jsx
@@ -0,0 +1,9 @@
+import './rightBar.scss'
+const RightBar = () => {
+  return (
+    <div className='rightBar'>
+      
+    </div>
+  )
+}
+export default RightBar
